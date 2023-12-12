6fe117bd1b3758a2f1ba753411e5ca660c3eeafd
dreamjz <25699818+dreamjz@users.noreply.github.com>
2023-12-13 05:26:23 +0800

    feat(hanitv): download playlist

--- internal/resolvers/hanimetv/hanimetv.go
+++ internal/resolvers/hanimetv/hanimetv.go
@@ -10,6 +10,12 @@ import (
 	"strings"
 	"time"
 
+	"github.com/charmbracelet/log"
+
+	"golang.org/x/net/html"
+
+	"github.com/acgtools/hanime-hunter/pkg/util"
+
 	"github.com/acgtools/hanime-hunter/internal/resolvers"
 )
 
@@ -28,6 +34,10 @@ type resolver struct{}
 const videoAPIURL = "https://hanime.tv/api/v8/video?id="
 
 func (re *resolver) Resolve(u string, opt *resolvers.Option) ([]*resolvers.HAnime, error) {
+	if strings.Contains(u, "playlists") {
+		return resolvePlaylist(u)
+	}
+
 	urlRes, err := url.Parse(u)
 	if err != nil {
 		return nil, fmt.Errorf("parse %q: %w", u, err)
@@ -44,15 +54,21 @@ func (re *resolver) Resolve(u string, opt *resolvers.Option) ([]*resolvers.HAnim
 		return nil, err
 	}
 
+	log.Infof("Anime found: %s, Searching episodes, Please wait a moment...", v.HentaiFranchise.Title)
+
 	res := make([]*resolvers.HAnime, 0)
+	episodes := make([]string, 0)
 
 	if !opt.Series {
-		vidMap := getVidMap(v)
+		vidMap, eps := getVidMap(v)
+
+		episodes = append(episodes, eps[0])
+		log.Infof("Episodes found: %#q", episodes)
 
 		res = append(res, &resolvers.HAnime{
 			URL:    u,
 			Site:   site,
-			Title:  v.HentaiVideo.Name,
+			Title:  v.HentaiFranchise.Title,
 			Videos: vidMap,
 		})
 
@@ -65,19 +81,86 @@ func (re *resolver) Resolve(u string, opt *resolvers.Option) ([]*resolvers.HAnim
 			return nil, err
 		}
 
-		vidMap := getVidMap(video)
+		vidMap, eps := getVidMap(video)
+
+		episodes = append(episodes, eps[0])
 
 		res = append(res, &resolvers.HAnime{
 			Site:   site,
-			Title:  video.HentaiVideo.Name,
+			Title:  video.HentaiFranchise.Title,
 			Videos: vidMap,
 		})
 	}
+
+	log.Infof("Episodes found: %#q", episodes)
+
 	return res, nil
 }
 
-func getVidMap(v *Video) map[string]*resolvers.Video {
+func resolvePlaylist(u string) ([]*resolvers.HAnime, error) {
+	slugs, err := getPlaylistSlugs(u)
+	if err != nil {
+		return nil, err
+	}
+
+	res := make([]*resolvers.HAnime, 0)
+
+	for _, s := range slugs {
+		v, err := getVideoInfo(s)
+		if err != nil {
+			return nil, err
+		}
+
+		log.Infof("Anime found: %s, Searching episodes, Please wait a moment...", v.HentaiFranchise.Title)
+
+		vidMap, eps := getVidMap(v)
+
+		log.Infof("Episodes found: %#q", eps[0])
+
+		res = append(res, &resolvers.HAnime{
+			Title:  v.HentaiFranchise.Title,
+			Videos: vidMap,
+		})
+
+		time.Sleep(time.Duration(util.RandomInt63n(900, 3000)) * time.Millisecond) //nolint:gomnd
+	}
+
+	return res, nil
+}
+
+func getPlaylistSlugs(u string) ([]string, error) {
+	doc, err := util.GetHTMLPage(NewClient(), u, map[string]string{"User-Agent": resolvers.UA})
+	if err != nil {
+		return nil, err
+	}
+
+	listDivs := util.FindTagByNameAttrs(doc, "div", true, []html.Attribute{{Key: "class", Val: "playlists__panel panel__content"}})
+	if len(listDivs) == 0 {
+		return nil, fmt.Errorf("playlist not found in %q", u)
+	}
+
+	aTags := util.FindTagByNameAttrs(listDivs[0], "a", true, []html.Attribute{{Key: "class", Val: "flex row"}})
+	if len(aTags) == 0 {
+		return nil, fmt.Errorf("anime not found in %q", u)
+	}
+
+	res := make([]string, 0)
+	for _, a := range aTags {
+		href := util.GetAttrVal(a, "href")
+		urlRes, _ := url.Parse(href)
+		path := urlRes.Path
+		if !strings.HasPrefix(path, "/videos/hentai/") {
+			continue
+		}
+		res = append(res, strings.TrimPrefix(path, "/videos/hentai/"))
+	}
+
+	return res, nil
+}
+
+func getVidMap(v *Video) (map[string]*resolvers.Video, []string) {
 	vidMap := make(map[string]*resolvers.Video)
+	eps := make([]string, 0)
 
 	for _, s := range v.VideosManifest.Servers[0].Streams {
 		if s.Height == "1080" {
@@ -85,18 +168,21 @@ func getVidMap(v *Video) map[string]*resolvers.Video {
 		}
 		quality := s.Height + "p"
 
+		eps = append(eps, v.HentaiVideo.Slug)
+		log.Debugf("video %s %s found", v.HentaiVideo.Slug, quality)
+
 		vidMap[quality] = &resolvers.Video{
 			ID:      strconv.FormatInt(s.ID, 10),
 			Quality: quality,
 			URL:     s.URL,
 			IsM3U8:  true,
-			Title:   v.HentaiVideo.Name,
+			Title:   v.HentaiVideo.Slug,
 			Size:    s.Size,
 			Ext:     "mp4",
 		}
 	}
 
-	return vidMap
+	return vidMap, eps
 }
 
 func getVideoID(path string) (string, error) {
